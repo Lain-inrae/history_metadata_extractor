5c31da926f391e47c74ac0c6394da728a0a7a7e5
Lain Pavot <lain.pavot@inrae.fr>
2022-07-12 13:19:45 +0200

    feat(inputs:dataset_attrs): add new input for better data processing.
    Great outputs, I think. Should be version 1.0.0 .

--- history_metadata_extractor.py
+++ history_metadata_extractor.py
@@ -6,17 +6,70 @@ import json
 PAGE_TEMPLATE = '\n'.join((
   "<!doctype HTML>",
   "<html>",
-  "{header}"
+  "{styles}",
+  "{header}",
+  """
+    <div
+      style="
+        position: fixed ;
+        top: 80px ;
+        left: 0px ;
+        color: white;
+        background-color: #2c3143;
+        z-index: 999 ;
+      "
+    >
+      <button
+        id="top"
+        class="btn btn-outline-light d-block m-2"
+      >Top</button>
+      <button
+        id="folder"
+        class="btn btn-outline-light d-block m-2"
+      >Fold/Unfold tables</button>
+      <button
+        id="toggle_deleted"
+        class="btn btn-outline-light d-block m-2"
+      >Show/Hide deleted</button>
+      <button
+        id="bottom"
+        class="btn btn-outline-light d-block m-2"
+      >Bottom</button>
+    </div>
+  """,
+  """
+  <nav
+    class="navbar justify-content-center navbar-dark navbar-expand"
+    id="masthead"
+    style="
+      background-color: #2c3143;
+      top:0px;
+      position:fixed;
+      width: 100%;
+      z-index: 999 ;
+    "
+  >
+    <a class="navbar-brand">
+      <span class="navbar-brand-title">{title}</span>
+    </a>
+  </nav>
+  """,
   "  <body",
   "    class=\"bg-secondary bg-gradient\"",
-  "    style=\"--bs-bg-opacity: .1;\"",
+  "    style=\"--bs-bg-opacity: .1; padding-top: 80px\"",
   "  >",
-  "    <div class=\"container bg-white\">"
-  "{styles}",
-  "{title}",
-  "{table_list}",
+  """
+      <div
+        id=\"content\"
+        class=\"container pt-2 bg-white\"
+        style=\"overflow-y: auto ; height: 100%\"
+      >
+  """,
+  "        {table_list}",
+  "      </div>"
   "    </div>"
   "  </body>",
+  "<script>{javascript}</script>",
   "</html>",
 ))
 
@@ -26,46 +79,77 @@ HEADER = """
 </head>
 """
 CSS_STYLES = """
-"""
-"""
 <style>
-table{
-  border: 1px solid;
-  border-collapse: separate;
-  border-radius: var(--elem-radius);
-  border-spacing: 0;
-  margin: 1rem 0;
-  width: 100%;
-  text-align: center;
-  box-sizing: border-box;
-  border-collapse: collapse;
+html, body {
+  height: 100% ;
+}
+</style>
+"""
+
+JAVASCRIPT = """
+
+var content = document.getElementById("content") ;
+document.getElementById("top").onclick = () => {
+  content.scrollTo(0, 0);
+} ;
+document.getElementById("bottom").onclick = () => {
+  content.scrollTo(0, content.scrollHeight);
+} ;
+var folded = false ;
+document.getElementById("folder").onclick = (e) => {
+  if (folded) {
+    var func = (item) => item.classList.remove("d-none") ;
+  } else {
+    var func = (item) => item.classList.add("d-none") ;
+  }
+  Array.prototype.forEach.call(
+    document.getElementsByClassName("table"),
+    func
+  ) ; 
+  folded = !folded ;
+}
+var show_deleted = true ;
+document.getElementById("toggle_deleted").onclick = (e) => {
+  show_deleted = !show_deleted ;
+  if (show_deleted) {
+    var func = (item) => item.classList.remove("d-none") ;
+  } else {
+    var func = (item) => item.classList.add("d-none") ;
+  }
+  Array.prototype.forEach.call(
+    document.getElementsByClassName("deleted"),
+    func
+  ) ; 
 }
 
-tr {
-  border: none;
-}
+Array.prototype.forEach.call(document.getElementsByTagName("h2"), (h2) => {
+  var table = h2.nextSibling.nextSibling.nextSibling.nextSibling ;
+  h2.onclick = (e) => {
+    if (table.classList.contains("d-none")) {
+      table.classList.remove("d-none")
+    } else {
+      table.classList.add("d-none")
+    }
+  } ;
+})
+"""
 
-td, th {
-  border-right: solid 1px #000;
-  border-left: solid 1px #000;
-}
-
-.even {
-  background-color: #e3ffe3
-}
-.odd {
-  background-color: #e3e3e3
-}
-</style>"""
-
-TITLE_TEMPLATE = "<h1>Galaxy {galaxy_version}</h1>"
+TITLE_TEMPLATE = "<h1>Galaxy - {galaxy_version}</h1>"
 
 TABLE_TEMPLATE = '\n'.join((
-  "<h2>{tool_id}</h2>",
-  "<h3>{tool_output}</h3>",
-  "<table class=\"table table-dark table-striped table-hover\">",
+  "<div class=\"{classes}\">"
+  """
+<h2
+  style=\"cursor: pointer\"
+  title=\"click to fold/unfold the table\"
+>{tool_name}</h2>
+""",
+  "<p>Outputs: {tool_output}</p>"
+  "<p>Status: {tool_status}</p>"
+  "<table class=\"table table-bordered table-dark table-striped table-hover d-block\">",
   "{table}",
   "</table>",
+  "</div>"
 ))
 
 HEADER_LIST_TEMPLATE = '\n'.join((
@@ -86,7 +170,15 @@ TABLE_LINE_TEMPLATE = "<td class=\"\">{}</td>"
 
 INDENT = "  "
 
-GLOBAL_CACHE = {}
+
+JOB_CACHE = {}
+HISTORY_CACHE = {}
+DATASET_CACHE = {}
+GLOBAL_CACHE = {
+  "jobs": JOB_CACHE,
+  "history": HISTORY_CACHE,
+  "dataset": DATASET_CACHE,
+}
 
 def indent(text):
   if text.startswith("\n"):
@@ -94,65 +186,109 @@ def indent(text):
   else:
     return INDENT+text.replace("\n", f"\n{INDENT}")
 
-def convert_to_html(objects):
+def noempty(ls, as_list=True):
+  if as_list:
+    return [x for x in ls if x]
+  return (x for x in ls if x)
+
+def join_noempty(ls, sep=';'):
+  return sep.join(noempty(ls, as_list=False))
+
+def extract_dataset_attributes(dataset_attrs):
+  for dataset_attr in dataset_attrs:
+    HISTORY_CACHE[dataset_attr["encoded_id"]] = dataset_attr
+  HISTORY_CACHE["dataset_attrs"] = dataset_attrs
+
+def convert_to_html(jobs_attrs, dataset_attrs=None):
+  if dataset_attrs:
+    extract_dataset_attributes(dataset_attrs)
   return PAGE_TEMPLATE.format(
     header=HEADER,
     styles=CSS_STYLES.replace("\n<", "\n  <"),
-    title=indent(indent(get_title(objects))),
-    table_list=indent(indent(get_table_list(objects)))
+    javascript=JAVASCRIPT,
+    title=indent(indent(get_title(jobs_attrs))),
+    table_list=indent(indent(get_table_list(jobs_attrs)))
   )
 
-def get_title(objects):
-  galaxy_version = objects[0].get("galaxy_version", "Unknown version")
+def get_title(jobs_attrs):
+  galaxy_version = jobs_attrs[0]["galaxy_version"] or "Unknown version"
   return TITLE_TEMPLATE.format(galaxy_version=galaxy_version)
 
-def get_table_list(objects):
+def get_table_list(jobs_attrs):
   return '\n'.join((
-    convert_item_to_table(obj)
-    for obj in objects
+    convert_item_to_table(job_attr, dataset_id)
+    for job_attr in jobs_attrs
+    for dataset_id_set in job_attr["output_dataset_mapping"].values()
+    for dataset_id in dataset_id_set
   ))
 
-def convert_item_to_table(obj):
-  output_hid = obj.get("encoded_id")
-  if not output_hid:
-    print(obj)
-    if obj["output_datasets"]:
-      output_hid = ';'.join(map(str, sorted(obj["output_datasets"])))
-    else:
-      output_hid = "Unknown"
+def convert_item_to_table(job_attr, dataset_id):
+  # if "dataset_attrs" in GLOBAL_CACHE:
+  #   encoded_jid = GLOBAL_CACHE["dataset_attrs"][index]["hid"]
+  # else:
+  encoded_jid = job_attr.get("encoded_id")
+  history = HISTORY_CACHE.get(dataset_id, {})
+  hid = history.get("hid", "DELETED")
+  if job_attr["exit_code"] == 0:
+    status = "Ok"
+    classes = "alert alert-success"
+  else:
+    status = "Failed"
+    classes = "alert alert-danger"
+  if hid == "DELETED":
+    classes += " deleted"
+  tool_name = job_attr["tool_id"]
+  if tool_name.count("/") >= 4:
+    tool_name = job_attr["tool_id"].split("/")[-2]
+  tool_name = tool_name + " - " + job_attr["tool_version"]
+  # print(GLOBAL_CACHE)
+  # print(encoded_jid)
+  tool_name = f"[{hid}] - {tool_name}"
   return TABLE_TEMPLATE.format(
-    tool_id=obj["tool_id"],
-    tool_output=output_hid,
-    table=convert_parameters_to_html(obj)
+    classes=classes,
+    tool_name=tool_name,
+    tool_output="",
+    tool_status=status,
+    table=convert_parameters_to_html(job_attr)
   )
 
-def convert_parameters_to_html(obj):
-  params = obj["params"]
-  params_enrichment(obj, params)
+def convert_parameters_to_html(job_attr):
+  params = job_attr["params"]
+  params_enrichment(job_attr, params)
   keys = [
     key for key in iter_parameter_keys(params)
     if key not in ("dbkey", "chromInfo", "__input_ext", "request_json")
   ]
   return '\n'.join((
-    indent(get_table_header(params, keys)),
+    # indent(get_table_header(params, keys)),
+    indent(get_table_header(params, ["value", "name", "extension", "hid"])),
     indent(get_table_lines(params, keys)),
   ))
 
-def params_enrichment(obj, params):
+def params_enrichment(job_attr, params):
   if (
     all(map(params.__contains__, ("request_json", "files")))
-    and "encoded_id" in obj
+    and "encoded_id" in job_attr
   ):
     params.update(json.loads(params.pop("request_json")))
     for i, target in enumerate(params.pop("targets")):
       files = target["elements"]
-      params["files"][i]["hid"] = ';'.join(str(file["object_id"]) for file in files)
-      params["files"][i]["name"] = ';'.join(str(file["name"]) for file in files)
-      params["files"][i]["extension"] = ';'.join(str(file["ext"]) for file in files)
-    GLOBAL_CACHE[obj["encoded_id"]] = {
-      "hid": ';'.join(file["hid"] for file in params["files"]),
-      "name": ';'.join(file["name"] for file in params["files"]),
-      "extension": ';'.join(file["extension"] for file in params["files"]),
+      params["files"][i]["hid"] = join_noempty(
+        str(file["object_id"])
+        for file in files
+      )
+      params["files"][i]["name"] = join_noempty(
+        str(file["name"])
+        for file in files
+      )
+      params["files"][i]["extension"] = join_noempty(
+        str(file["ext"])
+        for file in files
+      )
+    JOB_CACHE[job_attr["encoded_id"]] = {
+      "hid": join_noempty(file["hid"] for file in params["files"]),
+      "name": join_noempty(file["name"] for file in params["files"]),
+      "extension": join_noempty(file["extension"] for file in params["files"]),
     }
 
 def iter_parameter_keys(params):
@@ -198,20 +334,20 @@ def get_table_lines(params, keys):
 
 def table_lines_iterator(params, param_names):
   keys = ("value", "name", "extension", "hid",)
-  for key in keys:
+  for param_name in param_names:
     classes = ""
     table_lines = []
-    for param_name in param_names:
-      subparam = params
-      while '.' in param_name:
-        subkey, param_name = param_name.split('.', 1)
-        subparam = subparam[subkey]
+    subparam = params
+    while '.' in param_name:
+      subkey, param_name = param_name.split('.', 1)
+      subparam = subparam[subkey]
+    for key in keys:
       param = extract_param_info(key, subparam[param_name])
       table_lines.append(param)
     yield TABLE_LINE_LIST_TEMPLATE.format(
       classes=classes,
       table_lines=(
-        indent(COLUMN_TEMPLATE.format(key) + '\n')
+        indent(COLUMN_TEMPLATE.format(param_name) + '\n')
         + indent('\n'.join(map(
           TABLE_LINE_TEMPLATE.format,
           table_lines
@@ -225,7 +361,7 @@ def extract_param_info(key, param):
   if isinstance(param, dict) and key in param:
     return str(param[key])
   if isinstance(param, list):
-    return ';'.join(extract_param_info(key, p) for p in param)
+    return join_noempty(extract_param_info(key, p) for p in param)
   return ""
 
 def extract_param_value(param):
@@ -240,20 +376,20 @@ def extract_param_value(param):
       for file_id in param["values"]:
         file_id = file_id["id"]
         ids.append(file_id)
-        if file_id in GLOBAL_CACHE:
-          file_info = GLOBAL_CACHE[file_id]
+        if file_id in DATASET_CACHE:
+          file_info = DATASET_CACHE[file_id]
           param["name"] = file_info["name"]
           param["hid"] = file_info["hid"]
           param["extension"] = file_info["extension"]
-      return ';'.join(ids)
+      return join_noempty(ids)
   if isinstance(param, (str, int, float)):
     return str(param)
   if isinstance(param, (list, tuple)):
-    return ';'.join(map(extract_param_value, param))
+    return join_noempty(map(extract_param_value, param))
   return str(param)
 
 def join_dict_key_values(dico, ignore=()):
-  return ';'.join(
+  return join_noempty(
     f"{name}: {dico[name]}"
     for name in dico
     if name not in ignore
@@ -263,11 +399,18 @@ if __name__ == "__main__":
   import optparse
   parser = optparse.OptionParser()
   parser.add_option(
-    "-i", "--input",
-    dest="input",
+    "-j", "--jobs-attrs",
+    dest="jobs_attrs",
     help="write report of FILE",
     metavar="FILE",
     default="jobs_attrs.txt"
+  )
+  parser.add_option(
+    "-d", "--dataset-attrs",
+    dest="dataset_attrs",
+    help="extract additional info from this file",
+    metavar="FILE",
+    default=None,
   )
   parser.add_option(
     "-o", "--output",
@@ -278,11 +421,14 @@ if __name__ == "__main__":
   )
 
   (options, args) = parser.parse_args()
-  with open(options.input) as j:
-    objects = json.load(j)
+  with open(options.jobs_attrs) as j:
+    jobs_attrs = json.load(j)
 
-  objects = sorted([{
-    key: obj.get(key)
+  with open(options.dataset_attrs) as ds:
+    dataset_attrs = json.load(ds)
+
+  jobs_attrs = [{
+    key: jobs_attr.get(key)
     for key in (
       "galaxy_version",
       "tool_id",
@@ -290,10 +436,12 @@ if __name__ == "__main__":
       "encoded_id",
       "params",
       "output_datasets",
+      "exit_code",
+      "output_dataset_mapping",
     )
-  } for obj in objects],
-    key=lambda x:x.get("output_datasets", [-1])[0]
-  )
+  } for jobs_attr in jobs_attrs]
+  if jobs_attrs and jobs_attrs[0].get("output_datasets"):
+    jobs_attrs = sorted(jobs_attrs, key=lambda x:x["output_datasets"][0])
 
   with open(options.output, "w") as o:
-    o.write(convert_to_html(objects))
+    o.write(convert_to_html(jobs_attrs, dataset_attrs=dataset_attrs))
